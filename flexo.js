e869df546b80a55cc30769ffe0a1cf924c7fcf55
Stuart Sackler <stuart@sackler.ca>
2019-03-28 00:11:20 -0400

    initial upload

---
+++ flexo.js
@@ -0,0 +1,28 @@
+/*
+http://johnny-five.io/examples/flex/
+*/
+var five = require("johnny-five"),
+  board, flex;
+
+board = new five.Board();
+
+board.on("ready", function() {
+
+  // Create a new `flex sensor` hardware instance.
+  flex = new five.Sensor({
+    pin: "A2",
+    freq: 250
+  });
+
+  // Inject the `sensor` hardware into
+  // the Repl instance's context;
+  // allows direct command line access
+  board.repl.inject({
+    pot: flex
+  });
+
+  // "data" get the current reading from the flex sensor
+  flex.on("data", function() {
+    console.log(this.value);
+  });
+});
